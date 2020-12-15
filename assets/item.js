d411ebde77eb410f52bdddd60a792c4cca48bce7
fantuotuo <635728757@qq.com>
2020-12-15 12:55:02 +0800

    Initial commit

---
+++ assets/item.js
@@ -0,0 +1,76 @@
+
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        uRank: {
+            get() { 
+                return this._uRank;
+            },
+            set(v) { 
+                this._uRank = v;
+                // todo
+                this.node.getChildByName("rank").getComponent("cc.Label").string = v;
+            }
+        },
+        uAvatar: {
+            get() { 
+                return this._uAvatar;
+            },
+            set(v) { 
+                this._uAvatar = v;
+                // todo
+                var avatar = this.node.getChildByName("avatar").getComponent("cc.Sprite");
+                cc.loader.load({ url: v, type: 'jpg' }, function (err, tex) {
+                    avatar.spriteFrame = new cc.SpriteFrame(tex);
+                });
+            }
+        },
+        uName: {
+            get() { 
+                return this._uName;
+            },
+            set(v) { 
+                this._uName = v;
+                // todo
+                this.node.getChildByName("name").getComponent("cc.Label").string = v;
+            }
+        },
+        uScore: {
+            get() { 
+                return this._uScore;
+            },
+            set(v) { 
+                this._uScore = v;
+                // todo
+                this.node.getChildByName("score").getComponent("cc.Label").string = v + this.addon;
+            }
+        },
+
+        addon: cc.String,
+        
+    },
+
+
+    
+
+    init(rank, avatarUrl, name, score, addon) {
+        this.addon = addon ? addon : "";
+
+        this.uRank = rank;
+        this.uAvatar = avatarUrl;
+        this.uName = name;
+        this.uScore = score;
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
